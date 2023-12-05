f06591cdaaf5338a6d10d482ce8426d0604d2701
ChannMyaeAung <chanmyaeaung@my.jcu.edu.au>
2023-12-05 21:08:33 +0630

    installed framer-motion and apply animation to hero jsx component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../styles";
+import { motion } from "framer-motion";
+import { fadeIn } from "../animation";
 
 const Hero = () => {
   return (
@@ -7,7 +9,13 @@ const Hero = () => {
       className={`z-[1]  shadow-lg  md:flex-row-reverse md:flex md:items-start md:justify-between md:gap-10 md:px-3 md:h-[30rem] lg:px-10 lg:h-[40rem]`}
     >
       {/* Background and mockup images */}
-      <div className="relative ">
+      <motion.div
+        variants={fadeIn("left", 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: true, amount: 0.7 }}
+        className="relative "
+      >
         <img
           src="/bg-intro-mobile.svg"
           alt="intro-mobile"
@@ -24,10 +32,14 @@ const Hero = () => {
           alt="mockup-img"
           className="absolute top-0 md:w-[40rem]"
         />
-      </div>
+      </motion.div>
 
       {/* Intro title, description & a button */}
-      <div
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: true, amount: 0.7 }}
         className={`py-16 md:py-0 md:my-0 md:self-center mt-10 ${styles.flexCenter} md:items-start md:justify-start flex-col gap-3`}
       >
         <h1 className={`${styles.heading} text-justify md:text-left`}>
@@ -45,7 +57,7 @@ const Hero = () => {
         >
           Request Invite
         </button>
-      </div>
+      </motion.div>
     </div>
   );
 };
